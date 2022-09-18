f4b5ec8205d06954a8e6ee767f0d9757d0d804ea
Bryce <hyrix59@hotmail.com>
2022-09-19 06:55:52 +1000

    Added basic backend express server

--- gomoku-service/src/app.ts
+++ gomoku-service/src/app.ts
@@ -5,11 +5,12 @@ dotenv.config()
 
 const app: Express = express()
 const port = process.env.PORT
+app.use(express.json())
 
 app.get('/', (req: Request, res: Response) => {
     res.send('hello world...')
 })
 
 app.listen(port, () => {
-    console.log(`[server]: Server is running at https://localhost:${port}`)
+    console.log(`[server]: Server is running at http://localhost:${port}`)
 })
